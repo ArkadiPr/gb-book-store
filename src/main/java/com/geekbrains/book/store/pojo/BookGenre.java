edb493e408200a676bd51830a9f73e41ffb41334
FMW14 <fmw14@yandex.ru>
2020-08-20 14:53:54 +0300

    pagination on ng

---
+++ src/main/java/com/geekbrains/book/store/pojo/BookGenre.java
@@ -0,0 +1,17 @@
+package com.geekbrains.book.store.pojo;
+
+public class BookGenre {
+    private String name;
+
+    public BookGenre(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
